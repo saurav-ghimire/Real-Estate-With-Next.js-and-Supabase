fd2431a8832fe11b258018d03b30094bcacf8996
saurav-ghimire <sauravghimire0123@gmail.com>
2024-04-22 18:41:08 -0400

    Project Setup

---
+++ app/Provider.jsx
@@ -0,0 +1,5 @@
+function Provider({children}) {
+  return ( children );
+}
+
+export default Provider;
---
+++ app/_components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return ( "Header is here" );
+}
+
+export default Header;
